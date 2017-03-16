37d76bbf9603390f6b7ad1727f17d875fa1d59ad
vince <vinceskahan@gmail.com>
2017-03-16 15:06:40 -0700

    rename things, test ansible install, add packages

--- 000-FIRSTBOOT.sh
+++ 000-FIRSTBOOT.sh
@@ -7,7 +7,7 @@ cd /boot
 sudo apt-get update
 
 # this mess because debian has an ancient unsupported ansible
-sudo apt-get install -y python-setuptools
+sudo apt-get install -y python-setuptools python-dev build-essential libssl-dev libffi-dev 
 sudo easy_install pip
 sudo pip install ansible
 
